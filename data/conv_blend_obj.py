6b1e3026d5088894b00913cd10ebf54f31af09b7
Gerald Kimmersdorfer <gerald.kimmersdorfer@gmail.com>
2022-06-19 16:46:25 +0200

    adds shroom dataset

--- data/conv_blend_obj.py
+++ data/conv_blend_obj.py
@@ -1,24 +1,50 @@
 
-file = open("untitled.obj", "r")
+file = open("shroom_blender.obj", "r")
 lines = file.readlines()
 
+def combineToPolyLines(prim):
+    newPrimArray = []
+    while len(prim) > 0:
+        tmpPrim = prim.pop(0)
+        madeChanges = True
+        while madeChanges:
+            madeChanges = False
+            for line in prim:
+                if tmpPrim[0] == line[len(line)-1]:
+                    #line is a predecessor
+                    tmpPrim = line[0:len(line)-1] + tmpPrim
+                    prim.remove(line)
+                    madeChanges = True
+                if tmpPrim[len(tmpPrim) - 1] == line[0]:
+                    #line is a successor
+                    tmpPrim = tmpPrim + line[1:]
+                    prim.remove(line)
+                    madeChanges = True
+        newPrimArray.append(tmpPrim)
+    return newPrimArray
+
 count = 0
-vs = []
+vertices = []
+prim = []
 for line in lines:
-    if line.startswith("v "):
-        vs.append(line)
+    ss = line.strip().split(" ")
+    if ss[0] == "v":
+        vertices.append([float(ss[1]), float(ss[2]), float(ss[3])])
         count = count + 1
+    elif ss[0] == "l":
+        prim.append([int(ss[1])-1, int(ss[2])-1])
+
+polys = combineToPolyLines(prim)
 
 output = ""
-i = 0
-for v in vs:
-    output += v + "vt " + "{:.5f}".format(i/count) + "\n"
-    i = i+1
-    if i % int(count/10) == 0:
-        output += "l\n"
-    
-output += "l\n"
+for pline in polys:
+    for vId in pline:
+        vertex = vertices[vId]
+        output += f"v {vertex[0]:.8f} {vertex[1]:.8f} {vertex[2]:.8f}\n"
+        output += f"vt {vertex[2]}\n"
+    output += "l\n"
+
 file.close()
-file = open("test.obj", "w")
+file = open("shroom.obj", "w")
 file.write(output)
 file.close()
